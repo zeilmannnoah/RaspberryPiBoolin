fc8978e448314ea0f96c746fcec8b5b619dbf92f
Noah Z <zeilmannnoah@gmail.com>
2018-08-11 03:22:00 -0500

    Added restart command

--- src/main.js
+++ src/main.js
@@ -1,20 +1,28 @@
 const Discord = require('discord.js');
 const Token = require('./token.js');
+const { exec } = require('child_process');
 const Client = new Discord.Client();
 
-console.log("change");
-
 Client.on('ready', () => {
-	console.log('Logged in as ${client.user.tag}!');
+	console.log('Logged in with token: ' + Token.value);
 });
 
 Client.on('message', msg => {
 	if (msg.content === 'ping') {
 		msg.reply('pong');
 	}
+	else if (msg.content === 'restart') {
+		exec('git pull', (err, stdout, stderr) => {
+			if(err) {
+				msg.reply(stderr);
+			}
+			else {
+				msg.reply("Pulling and restarting app");
+				console.log(stdout);
+			}
+		});
+	}
 });
-
-console.log("Loggin in with token: " + Token.value);
 
 Client.login(Token.value)
 .catch(err => {
