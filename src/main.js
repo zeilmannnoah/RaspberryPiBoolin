9cc97704e296883c98b08671f14f4f67b69aa5e3
Noah Zeilmann <zeilmannnoah@gmail.com>
2018-08-11 07:39:04 +0000

    inital commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const Token = require('./token.js');
+const Client = new Discord.Client();
+
+Client.on('ready', () => {
+	console.log('Logged in as ${client.user.tag}!');
+});
+
+Client.on('message', msg => {
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+	}
+});
+
+console.log("Loggin in with token: " + Token.value);
+
+Client.login(Token.value)
+.catch(err => {
+	console.log(err);
+});
---
+++ src/token.js
@@ -0,0 +1,1 @@
+module.exports={value:'NDc3NzM0NTU1OTkwODg0MzYy.DlAc2g.U4WYbvgM1YUaI_lGDMMP2RoNFPA'};
